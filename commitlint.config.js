babb52694ca3967eb74b98e0181ed8546b690308
Lenerystia <marlenamakosza@gmail.com>
2025-01-08 20:02:05 +0100

    (chore): Add plugin to commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 export default {
     extends: ['@commitlint/config-conventional'],
+	plugins: ['commitlint-plugin-function-rules'],
+	rules: {
+		// 'header-max-length': [0], // level: disabled
+	},
 }
